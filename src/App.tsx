caa69f4977c99710fc2f3495e3d151591df1703c
WhatWR <kul.b@ku.th>
2023-05-15 17:50:22 +0700

    list of course

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Schedule, { Course } from './Components/Schedule/Schedule'
 
 function App() {
+  const courses: Course[] = [
+    {
+      name: 'Math',
+      detail: 'Advanced calculus',
+      time: '9:00 AM - 10:30 AM',
+      day: 'Monday',
+      code: 'MATH101',
+    },
+    {
+      name: 'Physics',
+      detail: 'Quantum mechanics',
+      time: '11:00 AM - 12:30 PM',
+      day: 'Wednesday',
+      code: 'PHYS201',
+    },
+  ]
   return (
     <div className='App'>
-      <header className='App-header'>
+      {/* <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +35,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Schedule courses={courses} />
     </div>
   )
 }
